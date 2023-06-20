16f062e83e27d735904a86f5538f29408b2a2794
jayiterator <jayson.nm.paul@proton.me>
2023-06-20 16:15:33 +1000

    Integrating Chakra UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
-import Image from "next/image";
+"use client";
+import { Button } from "@chakra-ui/react";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-[#050017]">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark: bg-[#050017] dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Welcome home Jayson
+          Welcome home Jayson. This is your personal site.
+          <Button colorScheme="blue" variant="outline">
+            Button
+          </Button>
         </p>
       </div>
     </main>
